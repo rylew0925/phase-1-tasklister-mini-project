fbb879b04fb87be3a5ad220f6f77f0a5707d9e48
Richard <rlew0925@gmail.com>
2025-07-23 23:21:39 -0700

    Updated JavaScript file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+  const taskList = document.getElementById("tasks");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const input = document.getElementById("new-task-description");
+    const taskText = input.value;
+
+    const li = document.createElement("li");
+    li.textContent = taskText;
+
+    taskList.append(li);
+    input.value = ""; // Clear input after submission
+  });
 });
